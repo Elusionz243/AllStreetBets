b5ca0c1805829194ec52d40c64a6d05a863e8a14
Elusionz243 <strivelinkband@yahoo.com>
2024-05-25 22:46:11 -0500

    Hero complete for Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import "./App.scss";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ front-end/src/components/home/Home.js
@@ -0,0 +1,17 @@
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1 className="Home__title">FREE YOUR MIND</h1>
+      <div className="Home__content">
+        <a className="Home__bets-btn">GET $BETS ON BASE</a>
+        <p className="Home__description">
+          All Street Bets ($BETS) represents freedom through a decentralized
+          future. A culture coin brought to you by the founder of WallStreetBets
+          & the web3 community.
+        </p>
+      </div>
+    </div>
+  );
+}
